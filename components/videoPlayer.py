e09c097f7f98dc004cb45849fb17d0a6513a0881
UC-FAST <ywt-ywt@qq.com>
2022-06-02 19:32:25 +0800

    Bug fix

--- components/videoPlayer.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-class VideoPlayer:
-    def __init__(self,path,width,height):
-        self.__videoReader=cv2.VideoCapture(path)
-        self.__width,self.__height=width,height
-
-    def getFirstFrame(self):
-        pass
